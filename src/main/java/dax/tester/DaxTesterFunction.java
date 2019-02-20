006fc8c0d076a50cc501e7433a6eb4c9db49a569
Vladimir Orany <vladimir@orany.cz>
2019-02-20 11:52:24 +0100

    simplified function

--- src/main/java/dax/tester/DaxTesterFunction.java
+++ src/main/java/dax/tester/DaxTesterFunction.java
@@ -2,11 +2,9 @@ package dax.tester;
 
 import io.micronaut.function.FunctionBean;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.util.Objects;
 import java.util.function.Supplier;
+
+import static java.util.Objects.requireNonNull;
 
 @FunctionBean("dax-tester")
 public class DaxTesterFunction implements Supplier<String> {
@@ -21,18 +19,12 @@ public class DaxTesterFunction implements Supplier<String> {
     public String get() {
         long start = System.currentTimeMillis();
 
-        InetSocketAddress address = new InetSocketAddress("testcluster.fdble5.clustercfg.dax.euw1.cache.amazonaws.com", 8111);
 
-        try (Socket socket = new Socket()) {
-            socket.connect(address, 10000);
-        } catch (IOException e) {
-            return "cannot connect to the cluster";
-        }
-
-        testEntityService.save(new DaxTestEntity().withHashKey("hash").withRange("range"));
+        DaxTestEntity entity = new DaxTestEntity().withHashKey("hash").withRange("range");
+        testEntityService.save(entity);
 
         for (int i = 0; i < 1000 ; i++) {
-            Objects.requireNonNull(testEntityService.load("hash", "range"));
+            requireNonNull(testEntityService.load("hash", "range"));
         }
 
         long finish = System.currentTimeMillis();
